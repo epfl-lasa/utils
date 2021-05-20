898e41002fbdf958969efb025b937d3b4c25ebc7
David Gonon <david.gonon@epfl.ch>
2021-05-20 12:32:05 +0200

    eq-tex2png

---
+++ eq-tex2png/README.md
@@ -0,0 +1,9 @@
+# Eq-tex2png
+
+The utility converts Latex-snippets to PNG-images. It processes all the files in a given directory, where each file contains bare Latex-code (without need for any preamble or environment definitions). The way to call the utility is as follows.
+```
+. path/to/eq-tex2png/compile path/to/equations-directory
+```
+This creates the PNG-images in the directory where it is executed.
+
+Author: David J. Gonon
---
+++ eq-tex2png/compile.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+EQUATIONS_DIR="$1"
+EQUATIONS_DIR=${EQUATIONS_DIR%/}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+for fullfilename in "${EQUATIONS_DIR}/"*; do
+	if [[ -d $fullfilename ]]; then
+		echo "Skipping directory: $fullfilename"
+		continue
+	fi
+	filename_with_extension=$(basename -- "$fullfilename")
+	echo "Found: $filename_with_extension"
+	cat "${SCRIPT_DIR}/prefix.tex" > tmp.tex
+	cat "$fullfilename" >> tmp.tex
+	cat "${SCRIPT_DIR}/suffix.tex" >> tmp.tex
+	pdflatex -shell-escape -interaction=nonstopmode -file-line-error tmp.tex | grep ".*:[0-9]*:.*"
+	filename="${filename_with_extension%.*}"
+	convert -density 1200 -depth 8 -quality 1200 tmp.pdf "${filename}.png"
+	echo "Wrote: ${filename}.png"
+done
+rm tmp.pdf tmp.aux tmp.log tmp.tex
